16de42730969d0809980f34ac99c01fa4d178d01
larocheleo <larocheleo@free.fr>
2024-09-12 11:26:36 +0200

    fonction sans **

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,5 +1,3 @@
-#créer une fonction puissance prenant 2 arguments entiers a et b et qui va renvoyer le nombre entier ab.
-
 def puissance(a,b):
 	if not type(a) is int:
 		raise TypeError("Only integers are allowed")
