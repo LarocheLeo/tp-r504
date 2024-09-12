abe2174ef581708cd76e24c357736fbd43203b16
larocheleo <larocheleo@free.fr>
2024-09-12 10:32:53 +0200

    cas limites

--- TP1/test.py
+++ TP1/test.py
@@ -8,3 +8,10 @@ def test_1 () :
 def test_2 () :
 	assert f.puissance(-2,3)==-8
 	assert f.puissance(2,-2)==0.25
+	
+def test_3():
+    with pytest.raises(ZeroDivisionError):
+        f.puissance(0,-2)
+        f.puissance(0,-5)
+        f.puissance(0,-9)
+
