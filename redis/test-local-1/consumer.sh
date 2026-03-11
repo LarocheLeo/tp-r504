67290deb3371d7276319fe4525a06fed230ac1a1
Leo <larocheleo@free.fr>
2026-03-11 16:30:48 +0100

    Mini projet part 1

---
+++ redis/test-local-1/consumer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+threshold=30000      # seuil d’alarme
+delay_process=2      # pause en cas d’alarme
+empty_wait=2         # attente si liste vide
+
+redis-cli DBSIZE >/dev/null
+if ! [ $? = 0 ]
+then
+	echo "Erreur, pas de connection avec le serveur redis !"
+	exit 1
+fi
+
+while true 
+do
+	# Récupération de la taille de la liste
+	nb=$(redis-cli --raw LLEN listproject)
+    if [ $nb -gt 0 ]
+    then
+        # Dépiler un élément
+        value=$(redis-cli --raw RPOP listproject)
+
+        echo "Valeur dépilée : $value"
+
+        # Test du seuil
+        if [ $value -gt $threshold ]
+        then
+            echo "ALARME ! Valeur = $value"
+            sleep $delay_process
+        fi
+
+    else
+        echo "Liste vide, attente 2s."
+        sleep 2
+    fi
+done
+
